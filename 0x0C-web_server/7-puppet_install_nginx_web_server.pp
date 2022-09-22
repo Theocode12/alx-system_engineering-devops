69d8e64d755fe9790a2fedfe8bb1f9aad781b455
theocode12 <vchidubem9@gmail.com>
2022-09-22 02:56:08 +0100

    puppet_nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,15 +1,7 @@
 # A puppet manifest to configure ubuntu
+nstalls a Nginx server
 
-file { 'index.html':
-  path => '/var/www/html/index.html',
-  content => 'Hello World!',
-  ensure => 'present',
-}
-
-exec { 'install nginx':
-  command => "/bin/bash apt-get update;/bin/bash apt-get install nginx",
-}
-
-exec { 'redirect':
-  command => '/usr/bin/sed -i "/server_name _;/a \\\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/odelolajosh;\n\t}" /etc/nginx/sites-available/default; service nginx start',
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/odelolajosh permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
