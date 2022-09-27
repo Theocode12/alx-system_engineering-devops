5ffe4add512d2346e569dcb5959e78e87fb19eb6
theocode12 <vchidubem9@gmail.com>
2022-09-27 06:00:05 +0100

    taga

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,7 @@
+# A puppet manifest to configure ubuntu
+# installs a Nginx server
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/odelolajosh permanent;/" /etc/nginx/sites-available/default;sudo sed -i "/server_name _;/a \\\n\tadd_header X-serverd-By: $(hostname);"; sudo service nginx start',
+}
