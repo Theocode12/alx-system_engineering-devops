8e622c7269df524786a3ff5ad82228f68ce38829
Theocode12 <vchidubem9@gmail.com>
2022-12-15 03:43:10 +0100

    server fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #A file to correct the 500 server error
 
 exec { 'correct-spelling erroe':
-    command => 'sed -i s/phpp/php/g "/var/www/html/wp-settings.php"'
+    command => '/bin/sed -i s/phpp/php/g "/var/www/html/wp-settings.php"'
 }
