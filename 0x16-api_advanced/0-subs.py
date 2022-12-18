b5ec93393989ce38702a372d8d25d529651841b8
Theocode12 <vchidubem9@gmail.com>
2022-12-18 09:46:54 +0100

    use of Reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module that returns the number of subscribers for a given subreddit"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+    headers = {'X-Modhash': 'Alx student subscribers', 'User-Agent': 'Alx subscriber task v1.01'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        return 0
+    return resp.json()['data']['subscribers']
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that return the first 10 hot posts"""
+import requests
+
+def top_ten(subreddit):
+    """Return the top 10 hot posts"""
+    headers = {'X-Modhash': 'Alx student subscribers', 'User-Agent': 'Alx subscriber task v1.01'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    params = {'limit': 10}
+    resp = requests.get(url, headers=headers, params=params)
+    if resp.status_code != 200:
+        print('None')
+        return
+    children = resp.json()['data']['children']
+    for child in children:
+        print(child['data']['title'])
