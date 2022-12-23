eb7510043d7d69ec28f4aa3f1db78ddb147f67fe
Theocode12 <vchidubem9@gmail.com>
2022-12-23 20:13:05 +0100

    ulimit fix'

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix for file limit(ulimit) tasks
 
 exec { 'ulimit fix':
-   command => '/bin/sed -i "s/ULIMIT/# ULIMIT/g" "/etc/default/nginx/"; /usr/sbin/service nginx restart'
+  command => '/bin/sed -i "s/ULIMIT/# ULIMIT/g" "/etc/default/nginx"; /usr/sbin/service nginx restart'
 }
